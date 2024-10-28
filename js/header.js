a652759e2b9d9f485efdc527343aa044d0fdacf3
Daniel-GC-23 <aloga.daniel.correa@gmail.com>
2024-10-28 10:16:37 -0600

    Footer bug fixes2

--- js/header.js
+++ js/header.js
@@ -1,32 +1,32 @@
 let contBut = 0;
 function headersHTML(headerElement) {
-    let borrar = headerElement.querySelector("#menuButton");
-    let collapsedMenu = headerElement.querySelector("#optionsMenu");
-    let expandedMenu = headerElement.querySelector("#dropdownMenu");
+    // let borrar = headerElement.querySelector("#menuButton");
+    // let collapsedMenu = headerElement.querySelector("#optionsMenu");
+    // let expandedMenu = headerElement.querySelector("#dropdownMenu");
 
-    let contenido =  document.getElementById("content")
+    // let contenido =  document.getElementById("content")
 
-    borrar.addEventListener('click', () => {
-        if (contBut == 1) {
-            expandedMenu.style.display = "none"
-            borrar.innerHTML = "<i id='botonmenu' class='bx bx-menu'></i>";
-            contBut = 0
-        } else {
-            expandedMenu.style.display = "block"
-            borrar.innerHTML = "<i id='botonx' class='bx bx-x'></i>";
-            contBut = 1
-        }
-    });
+    // borrar.addEventListener('click', () => {
+    //     if (contBut == 1) {
+    //         expandedMenu.style.display = "none"
+    //         borrar.innerHTML = "<i id='botonmenu' class='bx bx-menu'></i>";
+    //         contBut = 0
+    //     } else {
+    //         expandedMenu.style.display = "block"
+    //         borrar.innerHTML = "<i id='botonx' class='bx bx-x'></i>";
+    //         contBut = 1
+    //     }
+    // });
 
-    let buscar = headerElement.querySelector("#searchButton");
-    let inputSearch = headerElement.querySelector("#input-search");
-    let contentSearch = headerElement.querySelector("#div-Busqueda");
+    // let buscar = headerElement.querySelector("#searchButton");
+    // let inputSearch = headerElement.querySelector("#input-search");
+    // let contentSearch = headerElement.querySelector("#div-Busqueda");
 
-    buscar.addEventListener('click', () => {
-        inputSearch.style.display = "block"
-        contentSearch.style.backgroundColor = "rgb(233, 233, 233)";
-        contentSearch.style.animation = "scale-up-horizontal-right 0.4s";
-    });
+    // buscar.addEventListener('click', () => {
+    //     inputSearch.style.display = "block"
+    //     contentSearch.style.backgroundColor = "rgb(233, 233, 233)";
+    //     contentSearch.style.animation = "scale-up-horizontal-right 0.4s";
+    // });
 
     let aboutus = headerElement.querySelector("#aboutus");
     let computo1 = headerElement.querySelector("#computo1");
@@ -34,27 +34,26 @@ function headersHTML(headerElement) {
     let subscriptions = headerElement.querySelector("#subscriptions");
     let news = headerElement.querySelector("#news");
 
-
-    cargarContenido("./Pages/main.html");
+    cargarContenido("../Pages/main.html");
 
     aboutus.addEventListener("click", function() {
-        cargarContenido("./Pages/AboutUs/aboutus.html");
+        cargarContenido("../Pages/AboutUs/aboutus.html");
     });
 
     computo1.addEventListener("click", function() {
-        cargarContenido("./Pages/Computo/computo.html");
+        cargarContenido("../Pages/Computo/computo.html");
     });
 
     download.addEventListener("click", function() {
-        cargarContenido("./Pages/Download/download.html");
+        cargarContenido("../Pages/Download/download.html");
     });
 
     subscriptions.addEventListener("click", function() {
-        cargarContenido("./Pages/Subscriptions/subscriptions.html");
+        cargarContenido("../Pages/Subscriptions/subscriptions.html");
     });
 
     news.addEventListener("click", function() {
-        cargarContenido("./Pages/News/news.html");
+        cargarContenido("../Pages/News/news.html");
     });
 
 }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Cargar el header
-    fetch("./elements/header.html")
+    fetch("../elements/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-container").innerHTML = data;
             headersHTML(document.getElementById("header-container"));
         });
     // Cargar el footer
-    fetch("./elements/footer.html")
+    fetch("../elements/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
             footerHTML(document.getElementById("footer-container"));
         });
 
-        fetch("./elements/modal.html")
+        fetch("../elements/modal.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("modalStore").innerHTML = data;
