598db272dac8e1ec0683ffeee6ee5887c4968f65
Daniel-GC-23 <aloga.daniel.correa@gmail.com>
2024-11-05 11:40:27 -0600

    url changes

--- js/header.js
+++ js/header.js
@@ -8,38 +8,38 @@ function headersHTML(headerElement) {
     cargarContenido("../Pages/main.html", "/");
 
     aboutus.addEventListener("click", function () {
-        cargarContenido("../Pages/AboutUs/aboutus.html", "/aboutus");
+        cargarContenidohead("../Pages/AboutUs/aboutus.html", "/aboutus");
     });
 
     download.addEventListener("click", function () {
-        cargarContenido("../Pages/Download/download.html", "/download");
+        cargarContenidohead("../Pages/Download/download.html", "/download");
     });
 
     subscriptions.addEventListener("click", function () {
-        cargarContenido("../Pages/Subscriptions/subscriptions.html", "/subscriptions");
+        cargarContenidohead("../Pages/Subscriptions/subscriptions.html", "/subscriptions");
     });
 
     news.addEventListener("click", function () {
-        cargarContenido("../Pages/News/news.html", "/news");
+        cargarContenidohead("../Pages/News/news.html", "/news");
     });
 
     joinUs.addEventListener("click", function () {
-        cargarContenido("../Pages/Joinus/joinus.html", "/joinus");
+        cargarContenidohead("../Pages/Joinus/joinus.html", "/joinus");
         document.getElementById("header-container").style.display = "none";
         document.getElementById("footer-container").style.display = "none";
     });
 }
 
-function cargarContenido(url, path) {
+function cargarContenidohead(url, path) {
     fetch(url)
         .then(response => response.text())
         .then(data => {
             document.getElementById("content").innerHTML = data;
-            history.pushState(null, null, path); // Actualiza la URL sin recargar
+            history.pushState({}, "", path); // Actualiza la URL sin recargar
         });
 }
 
 // Manejo del botón "Atrás" en el navegador
 window.addEventListener("popstate", function() {
-    cargarContenido(window.location.pathname);
+    cargarContenidohead(window.location.pathname);
 });
